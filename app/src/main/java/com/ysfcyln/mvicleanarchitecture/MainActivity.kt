e1682b5feed5b7aa743612652186da2c100026d8
yusufceylan <ysf.cyln@yandex.com>
2021-01-30 20:40:58 +0300

    Create base module

--- app/src/main/java/com/ysfcyln/mvicleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/ysfcyln/mvicleanarchitecture/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.ysfcyln.mvicleanarchitecture
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.ysfcyln.base.BaseActivity
+import com.ysfcyln.mvicleanarchitecture.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override val bindLayout: (LayoutInflater) -> ActivityMainBinding
+        get() = ActivityMainBinding::inflate
+
+    override fun prepareView(savedInstanceState: Bundle?) {
+        // init views
     }
 }
