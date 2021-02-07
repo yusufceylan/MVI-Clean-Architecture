f78feab8ab044c5301f01755f617b1b351d77864
yusufceylan <ysf.cyln@yandex.com>
2021-02-07 17:08:39 +0300

    Create local module

---
+++ local/src/main/java/com/ysfcyln/local/source/LocalDataSourceImp.kt
@@ -0,0 +1,10 @@
+package com.ysfcyln.local.source
+
+import com.ysfcyln.data.repository.LocalDataSource
+import javax.inject.Inject
+
+/**
+ * Implementation of [LocalDataSource] Source
+ */
+class LocalDataSourceImp @Inject constructor() : LocalDataSource {
+}
