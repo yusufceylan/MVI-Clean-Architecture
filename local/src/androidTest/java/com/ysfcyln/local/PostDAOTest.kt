fde0621822a96d64c524735fca2973e87958bfa0
yusufceylan <ysf.cyln@yandex.com>
2021-02-17 18:57:53 +0300

    Test PostDAO with Dagger Hilt

--- local/src/androidTest/java/com/ysfcyln/local/PostDAOTest.kt
+++ local/src/androidTest/java/com/ysfcyln/local/PostDAOTest.kt
@@ -1,42 +1,41 @@
 package com.ysfcyln.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.google.common.truth.Truth
 import com.ysfcyln.local.database.AppDatabase
 import com.ysfcyln.local.database.PostDAO
 import com.ysfcyln.local.utils.TestDataGenerator
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
 @SmallTest
+@HiltAndroidTest
 class PostDAOTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var database : AppDatabase
+    @Inject
+    @Named("test_db")
+    lateinit var database : AppDatabase
     private lateinit var postDao : PostDAO
 
     @Before
     fun setUp() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            AppDatabase::class.java
-        )
-            .allowMainThreadQueries()
-            .build()
-
+        hiltRule.inject()
         postDao = database.postDao()
     }
 
