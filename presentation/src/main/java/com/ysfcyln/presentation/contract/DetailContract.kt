03cb98330e18c4493fa2a178113acbc46e3f6878
yusufceylan <ysf.cyln@yandex.com>
2021-03-13 21:34:52 +0300

    Create DetailContract.kt and DetailViewModel.kt

---
+++ presentation/src/main/java/com/ysfcyln/presentation/contract/DetailContract.kt
@@ -0,0 +1,34 @@
+package com.ysfcyln.presentation.contract
+
+import com.ysfcyln.base.UiEffect
+import com.ysfcyln.base.UiEvent
+import com.ysfcyln.base.UiState
+import com.ysfcyln.presentation.model.CommentUiModel
+import com.ysfcyln.presentation.model.PostUiModel
+
+/**
+ * Contract of Main Screen
+ */
+class DetailContract {
+
+    sealed class Event : UiEvent {
+        data class OnFetchPostComments(val post : PostUiModel) : Event()
+    }
+
+    data class State(
+        val commentsState: CommentsState,
+    ) : UiState
+
+    sealed class CommentsState {
+        object Idle : CommentsState()
+        object Loading : CommentsState()
+        data class Success(val comments : List<CommentUiModel>) : CommentsState()
+    }
+
+    sealed class Effect : UiEffect {
+
+        data class ShowError(val message : String?) : Effect()
+
+    }
+
+}
