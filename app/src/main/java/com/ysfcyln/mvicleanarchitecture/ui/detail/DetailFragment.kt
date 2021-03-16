633aa3ea2e60f0bb25519016fcd94a4135a40fdd
yusufceylan <ysf.cyln@yandex.com>
2021-03-16 21:35:16 +0300

    DetailFragment.kt and send argument

--- app/src/main/java/com/ysfcyln/mvicleanarchitecture/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/ysfcyln/mvicleanarchitecture/ui/detail/DetailFragment.kt
@@ -1,11 +1,18 @@
 package com.ysfcyln.mvicleanarchitecture.ui.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
 import com.ysfcyln.base.BaseFragment
 import com.ysfcyln.mvicleanarchitecture.databinding.FragmentDetailBinding
+import com.ysfcyln.presentation.contract.DetailContract
+import com.ysfcyln.presentation.vm.DetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 /**
  * Detail Fragment
@@ -13,11 +20,49 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding>() {
 
+    private val viewModel : DetailViewModel by viewModels()
+    private val args : DetailFragmentArgs by navArgs()
+
     override val bindLayout: (LayoutInflater, ViewGroup?, Boolean) -> FragmentDetailBinding
         get() = FragmentDetailBinding::inflate
 
     override fun prepareView(savedInstanceState: Bundle?) {
         // do something
+        initObservers()
+        val id = args.post?.id
+        Log.d("Ysf", id.toString())
+    }
+
+    /**
+     * Initialize Observers
+     */
+    private fun initObservers() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.uiState.collect {
+                when (val state = it.commentsState) {
+                    is DetailContract.CommentsState.Idle -> {
+                        Log.d("Ysf", "Idle")
+                    }
+                    is DetailContract.CommentsState.Loading -> {
+                        Log.d("Ysf", "Loading")
+                    }
+                    is DetailContract.CommentsState.Success -> {
+                        val data = state.comments
+                        Log.d("Ysf", "Success")
+                    }
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.effect.collect {
+                when (it) {
+                    is DetailContract.Effect.ShowError -> {
+                        val msg = it.message
+                    }
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/ysfcyln/mvicleanarchitecture/ui/main/MainFragment.kt
+++ app/src/main/java/com/ysfcyln/mvicleanarchitecture/ui/main/MainFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.ysfcyln.base.BaseFragment
 import com.ysfcyln.mvicleanarchitecture.databinding.FragmentMainBinding
 import com.ysfcyln.presentation.contract.MainContract
+import com.ysfcyln.presentation.model.PostUiModel
 import com.ysfcyln.presentation.vm.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
@@ -27,6 +29,12 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
     override fun prepareView(savedInstanceState: Bundle?) {
         initObservers()
         viewModel.setEvent(MainContract.Event.OnFetchPosts)
+
+        binding.tvSample.setOnClickListener {
+            val dummyPost = PostUiModel(1, 1, "title", "body")
+            val action = MainFragmentDirections.actionMainFragmentToDetailFragment().setPost(dummyPost)
+            findNavController().navigate(action)
+        }
     }
 
     /**
