6d8a5c736c7f411a835cf5a565967da66762709b
yusufceylan <ysf.cyln@yandex.com>
2021-03-27 13:15:58 +0300

    Create debug source set

---
+++ app/src/debug/java/com/ysfcyln/mvicleanarchitecture/HiltTestActivity.kt
@@ -0,0 +1,10 @@
+package com.ysfcyln.mvicleanarchitecture
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Test Activity, this activity will be used for Fragments to attach it
+ */
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
